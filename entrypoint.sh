2bd2cab0f4a60f1d938fab3fdda08c7fc4d48b8b
Mohammad hossain <mohammadhossain109@gmail.com>
2024-06-26 15:01:46 +0200

    file added

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-
+#touch /usr/local/apache2/htdocs/test.txt
 sed -i "s/{{TITLE}}/${TITLE}/g" /usr/local/apache2/htdocs/index.html
 sed -i "s|{{IMAGE}}|${IMAGE}|g" /usr/local/apache2/htdocs/index.html
 sed -i "s/{{NOM}}/${NOM}/g" /usr/local/apache2/htdocs/index.html
-exec "$@"
+#exec "$@"
